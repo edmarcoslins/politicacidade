803a8d4b91508aaba62dd488af0c8cba348bb3a4
edmarcoslins <elmf_lins@hotmail.com>
2017-12-01 02:44:14 -0300

    Path Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {RoutesNavigation} from './shared/route/routes-navigation';
+import {AuthComponent} from './auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: RoutesNavigation.LOGIN,
+    component: AuthComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
